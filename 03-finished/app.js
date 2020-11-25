255822e2d125f0874072cbbeb588752b09bb6a21
Daniel-Ok <daniel.okyere@amalitech.org>
2020-11-25 16:04:44 +0000

    first commit

---
+++ 03-finished/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  
+    res.send('<h1> Hello from Express!</h1>');
+});
+app.listen(3000);
---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+
+const server = http.createServer((rq,res) => {
+    console.log();
+
+});
+
+server.listen(3000);
