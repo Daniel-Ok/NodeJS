50b77738bda409be6042c8337b96babac70b3ba7
Daniel-Ok <daniel.okyere@amalitech.org>
2020-12-01 11:43:25 +0000

    added different routes

--- app.js
+++ app.js
@@ -38,15 +38,24 @@ const server = http.createServer((req,res) => {
 }); */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const server = http.createServer(app);
-app.use((req,res,next) =>{
-    console.log('In the middleware');
-    next();
-});
-app.use((req,res,next) => {
-    console.log('In the second middleware');
-    res.send('<h1>Hello from express</h1>')
-})
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-product',(req,res,next) => {
+  /*   console.log(req.body); */
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>');
+    
+});
+
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next) => {
+    /* console.log('In the second middleware'); */
+    res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000);
