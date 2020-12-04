6db6ae409a5587985da20a68dd1e94ffd10fa49f
Daniel-Ok <daniel.okyere@amalitech.org>
2020-12-04 16:12:58 +0000

    working on my routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const http = require('http');
+const http = require('http');
 
 
 const express = require('express');
@@ -12,9 +12,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
-
+app.use('/shop',shopRoutes);
 app.listen(3000);
